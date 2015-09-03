c383f99637048dae7462a0f72ab81f59c1abcbd4
Adham EL-Deeb <adham.eldeeb90@gmail.com>
2015-09-03 23:01:49 +0200

    show ruby version with dir with Gemfile only
    it's cool to show ruby version with directories that containing `Gemfile` only

--- fish_right_prompt.fish
+++ fish_right_prompt.fish
@@ -11,19 +11,21 @@ function _ruby_gemset
 end
 
 function fish_right_prompt
-  if [ "$theme_display_rbenv" = 'yes' ]
-    set -l red (set_color red)
-    set -l normal (set_color normal)
-
-    set ruby_info $red(_ruby_version)
-
-    if [ "$theme_display_rbenv_gemset" = 'yes' ]
-      if [ (_ruby_gemset) ]
-        set -l ruby_gemset $red(_ruby_gemset)
-        set ruby_info "$ruby_info@$ruby_gemset"
+  if test -f Gemfile
+    if [ "$theme_display_rbenv" = 'yes' ]
+      set -l red (set_color red)
+      set -l normal (set_color normal)
+  
+      set ruby_info $red(_ruby_version)
+  
+      if [ "$theme_display_rbenv_gemset" = 'yes' ]
+        if [ (_ruby_gemset) ]
+          set -l ruby_gemset $red(_ruby_gemset)
+          set ruby_info "$ruby_info@$ruby_gemset"
+        end
       end
+  
+      echo -n -s $ruby_info $normal
     end
-
-    echo -n -s $ruby_info $normal
   end
 end
